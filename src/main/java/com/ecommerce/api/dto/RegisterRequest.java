0088fa00c7e2b8d9c481cdc77ec30e1aedf05028
asmaSabr <asmasabrri@gmail.com>
2026-04-13 16:51:34 +0100

    feat: add RegisterRequest, LoginRequest and AuthResponse DTOs

---
+++ src/main/java/com/ecommerce/api/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.ecommerce.api.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String role;
+}
